b81938de6f3374e42d86babba7bb517701397574
devmustafashf1 <devmustafashf@gmail.com>
2026-05-30 16:33:57 +0500

    change to browser route from hashrouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
